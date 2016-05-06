de9d6658caf018495cb705e05c4a9b70a0b4d9b5
aun <aun.sira@gmail.com>
2016-05-06 13:34:44 +0700

    Initial commit

---
+++ loggy.rb
@@ -0,0 +1,82 @@
+require 'simple_stats'
+
+@hash_result = {
+    'GET /api/users/{user_id}/count_pending_messages' => {
+        dynos: {},
+        response_times: [],
+        count: 0
+    },
+    'GET /api/users/{user_id}/get_messages' => {
+        dynos: {},
+        response_times: [],
+        count: 0
+    },
+    'GET /api/users/{user_id}/get_friends_progress' => {
+        dynos: {},
+        response_times: [],
+        count: 0
+    },
+    'GET /api/users/{user_id}/get_friends_score' => {
+        dynos: {},
+        response_times: [],
+        count: 0
+    },
+    'POST /api/users/{user_id}' => {
+        dynos: {},
+        response_times: [],
+        count: 0
+    },
+    'GET /api/users/{user_id}' => {
+        dynos: {},
+        response_times: [],
+        count: 0
+    }
+}
+
+def find_max_dyno(dynos)
+  dynos.max_by { |k, v| v }
+end
+
+def extract_attributes
+  file = File.open('sample.log')
+  file.each_line do |f|
+    log = f.split /\s+/
+    @timestamp = log.shift
+    @heroku = log.shift
+    @log_level = log.shift
+    @method = log.shift.sub!('method=', '')
+    @path = log.shift.sub!('path=', '')
+    @host = log.shift
+    @fwd = log.shift
+    @dyno = log.shift.sub('dyno=', '')
+    @connect = log.shift.sub!('connect=', '')
+    @service = log.shift.sub!('service=', '')
+
+    # Replace user id with param
+    @path.gsub!(/\d+/, "{user_id}")
+
+    @hash_result.each do |key, value|
+      if key == "#{@method} #{@path}"
+        value[:count] += 1
+        value[:response_times] << @connect.to_i + @service.to_i
+        value[:dynos][@dyno] ||= 0
+        value[:dynos][@dyno] += 1
+      end
+    end
+  end
+end
+
+def shout_out
+  @hash_result.each do |key, value|
+    puts "URL => #{key} has been called #{value[:count]} times"
+    puts "Average Response time: #{value[:response_times].mean} ms"
+    puts "Median: #{value[:response_times].median} ms"
+    puts "Mode: #{value[:response_times].modes} ms"
+    puts "Most response dyno: #{find_max_dyno(value[:dynos])}"
+    puts "======================================================================="
+  end
+end
+
+extract_attributes
+shout_out
+
